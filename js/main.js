755c492aa56f12d1328a717f79d1056b00d4fc15
Atinad Hugo <danlinecrano@gmail.com>
2024-02-12 13:54:25 -0800

    working on Navbar

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//alert('we are Beta!!!');
+console.log('we are Beta!!!');
+
+var firstName = 'Atinad'
+
+var age = 9;
+
+let lastName = 'Hugo';
+const boilingWaterTemperature = 100;
+
+console.log(firstName);
+console.log(age);
+console.log(lastName);
+console.log(boilingWaterTemperature);
+boilingWaterTemperature = 120;
+
+//loops(for)
+for (let i = 1; i < 0; i) {
+
+}
